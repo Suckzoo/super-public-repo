e0876bbacd090e9a72fcf88abd1d16c032d449f3
Seokju Hong <seokju@vessl.ai>
2023-11-08 20:28:45 +0900

    logger

--- logger.py
+++ logger.py
@@ -3,9 +3,9 @@ import math
 
 vessl.init()
 for i in range(100):
-    s = sin(2*math.pi*i/100)
+    s = math.sin(2*math.pi*i/100)
     vessl.log({
         'sin_thing': 100*s*s,
-        'linear_thing': 100*i,
+        'linear_thing': i,
         'exp_thing': 100*(1-math.exp(-i/30)),
     })
